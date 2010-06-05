21508701c0e002c460d3fddf49fcd7723eb68148
Luca Ferroni <feroda@jagom.org>
2010-06-05 03:48:48 +0200

    Introduction page almost completed
    Updated stylesheets
    "What is this?" section
    Fortune application

--- settings.py
+++ settings.py
@@ -1,2 +1,14 @@
 from default_settings import *
+import os.path
 
+TIME_ZONE = "Europe/Rome"
+LANGUAGE_CODE = "it"
+SECRET_KEY = "ei1yaks0h54mrcxdrt=le!-%k%+^-nppuo5h9euywio(np5ge+"
+
+CONTACT_EMAIL = "info@jagom.org"
+SITE_NAME = "JAGOM"
+
+FORTUNE_BIN = "/usr/games/fortune"
+FORTUNE_PATH = os.path.join(PROJECT_ROOT, "media", "quotes")
+
+from final_settings import *
