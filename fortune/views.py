34be5d1c2f49383ae943f75f02489cd15d8351db
Luca Ferroni <fero@jagom.org>
2010-06-22 15:19:32 +0200

    Fixed intro page
    Italian localization
    Fixed empty fortunes
    Added "close" link to close "What is this" block

--- fortune/views.py
+++ fortune/views.py
@@ -5,7 +5,11 @@ from django.conf import settings
 
 def get_fortune(request):
 
-    msg = Popen([settings.FORTUNE_BIN, settings.FORTUNE_PATH], stdout=PIPE).communicate()[0]
+    msg = ""
+    while not msg:
+        # Avoid empty messages that are returned sometimes ...
+        msg = Popen([settings.FORTUNE_BIN, settings.FORTUNE_PATH], stdout=PIPE).communicate()[0]
+
     #FIXME: to be split and passed to template as context
     msg = '<div class="italic">' + msg
     msg = msg.replace("-- ", '</div><div class="author">')
