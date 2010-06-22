3531214eaa2fce296a268a2f4e10f2c214fdf64e
Luca Ferroni <fero@jagom.org>
2010-06-22 16:46:08 +0200

    Stub for about, terms and privacy policy

--- settings.py
+++ settings.py
@@ -5,7 +5,7 @@ TIME_ZONE = "Europe/Rome"
 LANGUAGE_CODE = "it"
 SECRET_KEY = "ei1yaks0h54mrcxdrt=le!-%k%+^-nppuo5h9euywio(np5ge+"
 
-CONTACT_EMAIL = "info@jagom.org"
+CONTACT_EMAIL = "chicitiene@jagom.org"
 SITE_NAME = "JAGOM"
 
 FORTUNE_BIN = "/usr/games/fortune"
