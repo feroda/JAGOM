f18bd96ac8f29cb53c6bc85680ac96622f53bda5
Luca Ferroni <fero@jagom.org>
2010-08-16 13:00:01 +0200

    Completed porting to pinax 0.9

--- urls.py
+++ urls.py
@@ -6,7 +6,7 @@ from django.contrib import admin
 admin.autodiscover()
 
 from tagging.models import TaggedItem
-from wiki.models import Article as WikiArticle
+from wakawaka.models import WikiPage
 
 from pinax.apps.account.openid_consumer import PinaxConsumer
 from pinax.apps.projects.models import Project
@@ -16,12 +16,6 @@ from pinax.apps.topics.models import Topic
 
 
 handler500 = "pinax.views.server_error"
-
-
-if settings.ACCOUNT_OPEN_SIGNUP:
-    signup_view = "pinax.apps.account.views.signup"
-else:
-    signup_view = "pinax.apps.signup_codes.views.signup"
 
 
 urlpatterns = patterns("",
@@ -34,24 +28,22 @@ urlpatterns = patterns("",
     }, name="home"),
     
     url(r"^admin/invite_user/$", "pinax.apps.signup_codes.views.admin_invite_user", name="admin_invite_user"),
-    url(r"^account/signup/$", signup_view, name="acct_signup"),
-    
-    (r"^about/", include("apps.about.urls")),
-    (r"^account/", include("pinax.apps.account.urls")),
-    (r"^openid/(.*)", PinaxConsumer()),
-    (r"^profiles/", include("pinax.apps.basic_profiles.urls")),
-    (r"^notices/", include("notification.urls")),
-    (r"^announcements/", include("announcements.urls")),
-    (r"^tagging_utils/", include("pinax.apps.tagging_utils.urls")),
-    (r"^comments/", include("threadedcomments.urls")),
-    (r"^attachments/", include("attachments.urls")),
-    
-    #(r"^groups/", include("basic_groups.urls")),
-    (r"^tribes/", include("pinax.apps.tribes.urls")),
-    (r"^projects/", include("pinax.apps.projects.urls")),
-    (r"^flag/", include("flag.urls")),
-    
-    (r"^admin/", include(admin.site.urls)),
+    url(r"^admin/", include(admin.site.urls)),
+    url(r"^about/", include("about.urls")),
+    url(r"^account/", include("pinax.apps.account.urls")),
+    url(r"^openid/(.*)", PinaxConsumer()),
+    url(r"^profiles/", include("idios.urls")),
+    url(r"^notices/", include("notification.urls")),
+    url(r"^announcements/", include("announcements.urls")),
+    url(r"^tagging_utils/", include("pinax.apps.tagging_utils.urls")),
+    url(r"^comments/", include("threadedcomments.urls")),
+    url(r"^attachments/", include("attachments.urls")),
+    url(r"^groups/", include("basic_groups.urls")),
+    url(r"^tribes/", include("pinax.apps.tribes.urls")),
+    url(r"^projects/", include("pinax.apps.projects.urls")),
+    url(r"^flag/", include("flag.urls")),
+
+    #WAS 0.9.dev12: url(r"^account/signup/$", signup_view, name="acct_signup"),
 
     url(r"^get_fortune/", "fortune.views.get_fortune", name="get_fortune"),
     url(r"^feed_proxy/", "feed_proxy.views.get_feed", name="feed_proxy"),
@@ -69,26 +61,26 @@ tagged_models = (
         query=lambda tag: TaggedItem.objects.get_by_model(Task, tag),
     ),
     dict(title="Wiki Articles",
-        query=lambda tag: TaggedItem.objects.get_by_model(WikiArticle, tag),
+        query=lambda tag: TaggedItem.objects.get_by_model(WikiPage, tag),
     ),
         
 
 )
 tagging_ext_kwargs = {
-  'tagged_models':tagged_models,
+  "tagged_models": tagged_models,
 }
 
-urlpatterns += patterns('',
-  url(r'^tags/(?P<tag>.+)/(?P<model>.+)$', 'tagging_ext.views.tag_by_model',
-        kwargs=tagging_ext_kwargs, name='tagging_ext_tag_by_model'),
-  url(r'^tags/(?P<tag>.+)/$', 'tagging_ext.views.tag',
-        kwargs=tagging_ext_kwargs, name='tagging_ext_tag'),
-  url(r'^tags/$', 'tagging_ext.views.index', name='tagging_ext_index'),
+urlpatterns += patterns("",
+  url(r"^tags/(?P<tag>.+)/(?P<model>.+)$", "tagging_ext.views.tag_by_model",
+        kwargs=tagging_ext_kwargs, name="tagging_ext_tag_by_model"),
+  url(r"^tags/(?P<tag>.+)/$", "tagging_ext.views.tag",
+        kwargs=tagging_ext_kwargs, name="tagging_ext_tag"),
+  url(r"^tags/$", "tagging_ext.views.index", name="tagging_ext_index"),
 )
 
 
 
 if settings.SERVE_MEDIA:
     urlpatterns += patterns("",
-        (r"", include("staticfiles.urls")),
+        url(r"", include("staticfiles.urls")),
     )
