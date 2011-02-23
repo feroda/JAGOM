0df957de2fd7f59a103b82d0f9bb3faff33096d5
Luca Ferroni <fero@jagom.org>
2011-02-23 18:18:24 +0100

    Fix on authentication check

--- apps/base/views.py
+++ apps/base/views.py
@@ -1,14 +1,15 @@
 from django.contrib import messages
 from django.shortcuts import render_to_response, get_object_or_404, redirect
+from django.template import RequestContext
 
 def get_messages(request):
     context = { 'messages' : messages.get_messages(request) }
     return render_to_response("base/messages.html", context)
 
 def home(request):
-    if request.user:
+    if request.user.is_authenticated():
         rv = redirect("your_projects")
     else:
-        rv = render_to_response("intro.html")
+        rv = render_to_response("intro.html", context_instance=RequestContext(request))
     return rv
         
